e022036efcf4742e69cc95943250664bb2c2892b
Nicholas Kawwas <68450354+nickawwas@users.noreply.github.com>
2020-09-10 23:34:06 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 "use strict";
 
-fetch("./cubing-data.json")
-  .then(function(resp) {
-    return resp.json();
-   })
-  .then(function(data) {
-   console.log(data);
-   });
+let alg = "R2 U' R2' S R2 S' U R2'";
+alg = alg.replace(/\s/g,''); 
+
+let sourced = `http://www.cubing.net/api/visualcube/?fmt=svg&amp;case= ${alg}`;
+<img src="sourced">
+// fetch("./cubing-data.json")
+//   .then(function(resp) {
+//     return resp.json();
+//    })
+//   .then(function(data) {
+//    console.log(data);
+//    });
