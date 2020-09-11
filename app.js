523c74d390a990546596e9d633e8c503cbd3cae4
Nicholas Kawwas <68450354+nickawwas@users.noreply.github.com>
2020-09-10 23:38:22 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ let alg = "R2 U' R2' S R2 S' U R2'";
 alg = alg.replace(/\s/g,''); 
 
 let sourced = `http://www.cubing.net/api/visualcube/?fmt=svg&amp;case= ${alg}`;
-<img src="sourced">
+console.log(`sourced`);
+<img src=`sourced`>
 // fetch("./cubing-data.json")
 //   .then(function(resp) {
 //     return resp.json();
