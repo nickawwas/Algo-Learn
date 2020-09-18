650cb6a4008f5c1638452fa6a8fd4adc94391f80
Nicholas Kawwas <68450354+nickawwas@users.noreply.github.com>
2020-09-17 21:59:25 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,46 @@
+function addImgs(algData, bldData) { //caseName
+  //Main Div
+  var addToDiv = document.getElementById("cubing-data");
+  
+  for (var i = 0; i < algData.length; i++) {
+    //Sub Div for Each Alg Case
+    var newDiv = document.createElement("div");
+    newDiv.id = "algs"; //"alg-" + i;
 
+    var newImg = new Image();
+    //newImg.id = "img-" + i;
+    newImg.src = "http://www.cubing.net/api/visualcube/?fmt=svg&case=" + algData[i].replace(/\s/g, '');	
+
+    // let algCase = document.createElement("p");
+    // algCase.textContent = caseName[i];
+    // algCase.id = "alg-case";
+
+    let regAlg = document.createElement("p");
+    regAlg.textContent = algData[i];
+    regAlg.id = "alg-moves";
+
+    let bldAlg = document.createElement("p");
+    bldAlg.textContent = bldData[i];
+    bldAlg.id = "bld-moves";
+
+    //Adds Elements to Sub Div	
+    //newDiv.appendChild(algCase);
+    newDiv.appendChild(newImg);
+    newDiv.appendChild(regAlg);
+    newDiv.appendChild(bldAlg);
+
+    //Adds Sub Div Into Main Div
+    addToDiv.appendChild(newDiv);
+  }
+}
+
+//Alg Data (Case Name, Blind and Reg Alg Notation)
+// let algCases = ["UB-UR (AB)", "UR-UB (BA)"];
+// let regAlgs = ["R2 U' S R2 S' R2 U R2'", "R2 U' R2' S R2 S' U R2'"];
+// let bldAlgs = ["[R2 U': [S, R2]]", "[R2 U': [R2', S]]"];
+
+const regAlgos = ["Lw' U R' D2 R U' R' D2' R Lw", "R' D' R U2 R' D R U R' D' R U' U2' R' D R", "F U2 R' D' R U2' R' D R F'", "R' D R U2 R' D' R U2'", "U' R D' R' U2 R D R' U2' U", "Lw' U2 R' D2 R U2' R' D2' R Lw", "D' R' D R U2 R' D' R U2' D", "D' U' R D' R' U2 R D R' U2' U D", "R D R' R' D2 R U2 R' D2' R U2' R D' R'", "U' R D R' U2 R D' R' U2' U", "R' D' R U2 R' D R U2'", "R' D' U2 R' D R U2' R' D' R D R", "R' D2 R U2 R' D2' R U2'", "D U' R D' R' U2 R D R' U2' U D'", "R F' R' U' R D R' U2 R D' R' U2' U R F R'", "D R D' R' R' D R U2 R' D' R U2' R D R' D'", "R D' R' R' D R U2 R' D' R U2' R D R'", "R' z R' R' D R U2 R' D' R U2' R z' R"];
+const bldAlgos = ["[Lw': [U, R' D2 R]]", "[R' D' R U2: [R' D R, U]]", "[F: [U2, R' D' R]]", "[R' D R, U2]", "[U': [R D' R', U2]]", "[Lw': [U2, R' D2 R]]", "[D': [R' D R, U2]]", "[D' U': [R D' R', U2]]", "[R D R': [R' D2 R, U2]]", "[U': [R D R', U2]]", "[R' D' R, U2]", "[R' D': [U2, R' D R]]", "[R' D2 R, U2]", "[D U': [R D' R', U2]]", "[R F' R' U': [R D R', U2]]", "[D R D' R': [R' D R, U2]]", "[R D' R': [R' D R, U2]]", "[R' z R': [R' D R, U2]]"];
+
+//Calling Function
+addImgs(regAlgos, bldAlgos); //algCases
