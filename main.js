3697dc5cf9f78517c161d6a0e38f78cb980819da
Nicholas Kawwas <68450354+nickawwas@users.noreply.github.com>
2020-09-22 18:56:58 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function addImgs(algData, bldData) { //caseName
+function addImgs(caseName, algData, bldData) { //caseName
   //Main Div
   let addToDiv = document.getElementById("cubing-data");
   
@@ -61,4 +61,4 @@ const regAlgos = ["Lw' U R' D2 R U' R' D2' R Lw", "R' D' R U2 R' D R U R' D' R U
 const bldAlgos = ["[Lw': [U, R' D2 R]]", "[R' D' R U2: [R' D R, U]]", "[F: [U2, R' D' R]]", "[R' D R, U2]", "[U': [R D' R', U2]]", "[Lw': [U2, R' D2 R]]", "[D': [R' D R, U2]]", "[D' U': [R D' R', U2]]", "[R D R': [R' D2 R, U2]]", "[U': [R D R', U2]]", "[R' D' R, U2]", "[R' D': [U2, R' D R]]", "[R' D2 R, U2]", "[D U': [R D' R', U2]]", "[R F' R' U': [R D R', U2]]", "[D R D' R': [R' D R, U2]]", "[R D' R': [R' D R, U2]]", "[R' z R': [R' D R, U2]]"];
 
 //Calling Function
-addImgs(regAlgos, bldAlgos); //algCases
+addImgs(algCases, regAlgos, bldAlgos);
