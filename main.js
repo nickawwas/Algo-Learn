6cfa17fada0ce8df40c2112a501618419934dafe
Nicholas Kawwas <68450354+nickawwas@users.noreply.github.com>
2020-09-22 18:56:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -11,9 +11,9 @@ function addImgs(algData, bldData) { //caseName
     //newImg.id = "img-" + i;
     newImg.src = "http://www.cubing.net/api/visualcube/?fmt=svg&case=" + algData[i].replace(/\s/g, '');	
 
-//     let algCase = document.createElement("p");
-//     algCase.textContent = caseName[i];
-//     algCase.id = "alg-case";
+    let algCase = document.createElement("p");
+    algCase.textContent = caseName[i];
+    algCase.id = "alg-case";
 
     let regAlg = document.createElement("p");
     regAlg.textContent = algData[i];
@@ -24,7 +24,7 @@ function addImgs(algData, bldData) { //caseName
     bldAlg.id = "bld-moves";
 
     //Adds Elements to Sub Div	
-    //newDiv.appendChild(algCase);
+    newDiv.appendChild(algCase);
     newDiv.appendChild(newImg);
     newDiv.appendChild(regAlg);
     newDiv.appendChild(bldAlg);
@@ -35,9 +35,27 @@ function addImgs(algData, bldData) { //caseName
 }
 
 // Alg Data (Case Name, Blind and Reg Alg Notation) 
-// let algCases = ["UB-UR (AB)", "UR-UB (BA)"]; 
 // let regAlgs = ["R2 U' S R2 S' R2 U R2'", "R2 U' R2' S R2 S' U R2'"]; 
 // let bldAlgs = ["[R2 U': [S, R2]]", "[R2 U': [R2', S]]"]; 
+
+ const algCases = ["ULB-URB (AB)",
+"ULB-ULF (AD)",
+"ULB-LFU (AF)", 
+"ULB-LDF (AG)", 
+"ULB-LBD (AH)", 
+"ULB-FUL (AI)", 
+"ULB-FDR (AK)", 
+"ULB-FLD (AL)",
+"ULB-RBU (AN)", 
+"ULB-RDB (AO)",
+"ULB-RFD (AP)", 
+"ULB-BUR (AQ)", 
+"ULB-BDL (AS)",
+"ULB-BDR (AT)", 
+"ULB-DFL (AU)",
+"ULB-DFR (AV)",
+"ULB-DBR (AW)",
+"ULB-DBL (AX)"];
 
 const regAlgos = ["Lw' U R' D2 R U' R' D2' R Lw", "R' D' R U2 R' D R U R' D' R U' U2' R' D R", "F U2 R' D' R U2' R' D R F'", "R' D R U2 R' D' R U2'", "U' R D' R' U2 R D R' U2' U", "Lw' U2 R' D2 R U2' R' D2' R Lw", "D' R' D R U2 R' D' R U2' D", "D' U' R D' R' U2 R D R' U2' U D", "R D R' R' D2 R U2 R' D2' R U2' R D' R'", "U' R D R' U2 R D' R' U2' U", "R' D' R U2 R' D R U2'", "R' D' U2 R' D R U2' R' D' R D R", "R' D2 R U2 R' D2' R U2'", "D U' R D' R' U2 R D R' U2' U D'", "R F' R' U' R D R' U2 R D' R' U2' U R F R'", "D R D' R' R' D R U2 R' D' R U2' R D R' D'", "R D' R' R' D R U2 R' D' R U2' R D R'", "R' z R' R' D R U2 R' D' R U2' R z' R"];
 const bldAlgos = ["[Lw': [U, R' D2 R]]", "[R' D' R U2: [R' D R, U]]", "[F: [U2, R' D' R]]", "[R' D R, U2]", "[U': [R D' R', U2]]", "[Lw': [U2, R' D2 R]]", "[D': [R' D R, U2]]", "[D' U': [R D' R', U2]]", "[R D R': [R' D2 R, U2]]", "[U': [R D R', U2]]", "[R' D' R, U2]", "[R' D': [U2, R' D R]]", "[R' D2 R, U2]", "[D U': [R D' R', U2]]", "[R F' R' U': [R D R', U2]]", "[D R D' R': [R' D R, U2]]", "[R D' R': [R' D R, U2]]", "[R' z R': [R' D R, U2]]"];
