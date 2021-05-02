6ed0cbefefafd81b0098028e1c8f447c29a58b37
Nicholas Kawwas <nicholas.b.kawwas@gmail.com>
2021-05-02 11:58:55 -0700

    Added Lazy Loading to Pages

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import React from 'react';
+import React, {lazy, Suspense} from 'react';
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 import Home from "./pages/Home/Home";
-import Mini from "./pages/Mini";
-import Regular from "./pages/Regular";
-import Blind from "./pages/Blind";
 import Contact from "./pages/Contact/Contact";
-
 import Navbar from './components/Navbar/Navbar';
 
 import "./App.css"
+
+/* Lazy Load Pages */
+const Mini = lazy(() => import("./pages/Mini"));
+const Regular = lazy(() => import("./pages/Regular"));
+const Blind = lazy(() => import("./pages/Blind"));
 
 const App = () => {
   return(
     <Router>
       <Navbar />
-      <Switch>
-        <Route path="/" exact component={Home}/>
-        <Route path="/2x2" component={Mini}/>
-        <Route path="/3x3" component={Regular}/>
-        <Route path="/3-bld" component={Blind}/>
-        <Route path="/contact" component={Contact}/>
-      </Switch>
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Suspense fallback={<div> Loading ... </div>}>
+            <Route path="/2x2" component={Mini}/>
+            <Route path="/3x3" component={Regular}/>
+            <Route path="/3-bld" component={Blind}/>
+          </Suspense>
+          <Route path="/contact" component={Contact}/>
+        </Switch>
     </Router>
   );
 }
